208fc3ebff545ea6014f7a3665f63a0ff94fa1dc
ldelgado12 <50750108+ldelgado12@users.noreply.github.com>
2019-05-22 11:33:01 -0600

    initialize google chrome
    class 13 of the course

---
+++ Demo.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//selenium code-
+		
+		//create driver object for chrome browser
+		
+		// we will strictly implement methods of webdriver
+		
+		/* class name = ChromeDriver, 
+		 * X driver= new X (); */
+		
+		//Invoke .exe file first
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ldelgado\\Documents\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.boxofficemojo.com/");
+		System.out.println(driver.getTitle());
+		
+		
+	}
+
+}
